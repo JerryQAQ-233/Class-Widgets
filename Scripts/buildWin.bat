2b2e3ccf14e5270e79015bc28bf2cd95cae5c2f6
Jerry233 <117577244+wangjinyi425@users.noreply.github.com>
2025-03-01 07:50:40 +0800

    Update buildWin.bat

--- Scripts/buildWin.bat
+++ Scripts/buildWin.bat
@@ -9,18 +9,10 @@ echo 打包
 python -m nuitka main.py ^
 --enable-plugin=pyqt5 ^
 --disable-console ^
---mode=app ^
+--mode=standalone ^
 -o"ClassWidgets" ^
 --windows-icon-from-ico=img/favicon.icns ^
 --product-name="Class Widgets" ^
 --product-version="1.1.7.1" ^
 --file-description="全新桌面课表" ^
---include-data-dir=img=img ^
---include-data-dir=ui=ui ^
---include-data-dir=view=view ^
---include-data-dir=config=config ^
---include-data-dir=plugins=plugins ^
---include-data-dir=font=font ^
---include-data-dir=audio=audio ^
---include-data-files=LICENSE=LICENSE ^
 --include-package=pyttsx3.drivers
